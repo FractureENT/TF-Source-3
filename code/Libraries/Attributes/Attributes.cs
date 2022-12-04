2abb5ac09869b130b4c7fe16af5adc3c35b3d162
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-04 13:17:31 +0200

    Added Attributes.NormalizeName

--- code/Libraries/Attributes/Attributes.cs
+++ code/Libraries/Attributes/Attributes.cs
@@ -1,6 +1,22 @@
-﻿namespace Amper.FPS;
+﻿using System.Text.RegularExpressions;
+
+namespace Amper.FPS;
 
 public class Attributes
 {
+	public static string NormalizeName( string attributeName )
+	{
+		// Trim the name to remove all the spaces.
+		attributeName = attributeName.Trim();
 
+		// Attribute names are always lowercase.
+		attributeName = attributeName.ToLower();
+
+		// Replace all spaces with underscores.
+		attributeName = attributeName.Replace( " ", "_" );
+
+		// Remove all unsupported characters.
+		attributeName = Regex.Replace( attributeName, "[^a-z0-9_]", " " );
+		return attributeName;
+	}
 }
