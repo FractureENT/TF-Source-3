5186ad815effb311a42b08fee03dc9fd41e1ef93
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-04 16:26:39 +0200

    IEconItem, IEconDefinition, IIndexedEconDefinition

---
+++ code/Libraries/Econ/IEconItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Amper.FPS;
+
+public interface IEconItem : IHasAttributes
+{
+	public int Quality { get; set; }
+	public EconItemDefinition ItemDefinition { get; set; }
+}
+
+public interface IEconDefinition
+{
+	public string ResourceName { get; }
+	public string ResourcePath { get; }
+}
+
+public interface IIndexedEconDefinition : IEconDefinition
+{
+	/// <summary>
+	/// Index that is used to identify this definition in the backend.<br/>
+	/// <b>NEVER CHANGE THIS VALUE AFTER THE DEFINITION WENT PUBLIC</b> 
+	/// </summary>
+	public int DefinitionIndex { get; }
+}
