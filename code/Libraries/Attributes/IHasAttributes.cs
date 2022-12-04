12eb14883a15a069c39776f1772d581245c1ca4c
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-04 11:12:58 +0200

    Added IHasAttributes

---
+++ code/Libraries/Attributes/IHasAttributes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS2;
+
+public interface IHasAttributes
+{
+	public Attributes Attributes { get; set; }
+}
