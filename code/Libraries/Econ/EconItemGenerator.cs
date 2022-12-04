271df37e4bca37569d948837358f86883b8a27b3
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-04 16:26:55 +0200

    Create EconItemGenerator.cs

---
+++ code/Libraries/Econ/EconItemGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace Amper.FPS;
+
+public class EconItemGenerator
+{
+	public static IEconItem GenerateItemFromResource( EconItemDefinition itemDef, int quality )
+	{
+		var engineClass = itemDef.EngineClass;
+
+		if ( string.IsNullOrEmpty( engineClass ) )
+			return null;
+
+		// Make sure the type we're working with exists.
+		if ( TypeLibrary.GetDescription( engineClass ) == null )
+			return null;
+
+		var item = TypeLibrary.Create<IEconItem>( engineClass );
+		if ( item == null )
+			return null;
+
+		item.ItemDefinition = itemDef;
+		item.Quality = quality;
+
+		return item;
+	}
+}
