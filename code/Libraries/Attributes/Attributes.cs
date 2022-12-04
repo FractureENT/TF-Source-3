767267bd25fb58a3464d824b63c5a54c19c4cd4d
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-12-04 14:03:52 +0200

    Attributes dictionary, mapped by StringTokens

--- code/Libraries/Attributes/Attributes.cs
+++ code/Libraries/Attributes/Attributes.cs
@@ -1,9 +1,13 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Sandbox;
 
 namespace Amper.FPS;
 
 public class Attributes
 {
+	private static Dictionary<StringToken, AttributeDefinition> DefinitionsByName = new();
+
 	public static string NormalizeName( string attributeName )
 	{
 		// Trim the name to remove all the spaces.
@@ -18,5 +22,38 @@ public class Attributes
 		// Remove all unsupported characters.
 		attributeName = Regex.Replace( attributeName, "[^a-z0-9_]", " " );
 		return attributeName;
+	}
+
+	public static AttributeDefinition GetDefinitionByName( string name )
+	{
+		name = NormalizeName( name );
+		if ( DefinitionsByName.TryGetValue( name, out AttributeDefinition attrDef ) )
+			return attrDef;
+
+		return null;
+	}
+
+	public static void RegisterDefinition( AttributeDefinition attrDef )
+	{
+		var name = NormalizeName( attrDef.ResourceName );
+		DefinitionsByName.Add( name, attrDef );
+	}
+
+	public void Set( string attributeName, string value )
+	{
+
+	}
+
+	[ConCmd.Server( "sv_test_attribute" )]
+	public static void Command_TestAttribute( string name )
+	{
+		var def = GetDefinitionByName( name );
+		if ( def == null )
+		{
+			Log.Info( "- Attribute not found." );
+			return;
+		}
+
+		Log.Info( "- Attribute Found: " + def.Type );
 	}
 }
